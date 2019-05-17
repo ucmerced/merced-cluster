67c0107262fa4e27f140e31bfbb525e5cda5a6a7
Samuel Araya <saraya209@users.noreply.github.com>
2019-05-16 21:59:21 -0700

    initial commit

---
+++ R-and-MPI/machine_learning_example.R
@@ -0,0 +1,54 @@
+## Machine Learning In Parallel With caret Example
+## Load libraries (must be installed beforehand)
+library(doMPI) ## Parallel computation package
+library(plyr) ## required for gbm
+library(caret) ## platform for training.
+library(gbm) ## gbm model
+#
+## (1) Create synthetic data, 5,000 rows, 3 predictor variables and 1 output variable
+n.rows = 5000
+n.cols = 4
+syn_data <- array(data = 0, dim = c(n.rows, n.cols))
+# Synthesize predictors
+syn_data[,2] <- c(seq(1, n.rows)) + rnorm(n.rows, mean = 0, sd = 20) # increaseing with normal errors added
+syn_data[,3] <- c(sin(rnorm(n.rows, sd = 20))) # decreasing with normal errors added
+syn_data[,4] <- c(runif(n.rows, 1, 50)) # random variable
+# calculate the output variable
+syn_data[,1] <- syn_data[,2] * syn_data[,3] + syn_data[,4]
+#
+## (2) Tune Model with caret package
+# Define tuning parameters for GBM model
+gbm_grid <- expand.grid(interaction.depth = 2:100,
+                        n.trees = floor((5:100)*50),
+                        shrinkage = .1,
+                        n.minobsinnode = 10)
+# Define training control methods
+ctrl_tr <- trainControl(method = "repeatedcv",
+                        number = 10,
+                        repeats = 5,
+                        returnResamp = "all")
+#
+## (3) Setup Parallel Proccessing
+ncores <- detectCores() # get the number of available cores
+cl <- startMPIcluster() # start a cluster
+registerDoMPI(cl) # register cluster to work on all cores
+# Document parallel processing variables
+print(ncores) # number of cores available and requested
+clusterSize(cl)
+#
+## (4) Run Model Training
+gbm_fit <- train(V1~., data = syn_data,
+                 method = "gbm",
+                 tuneGrid = gbm_grid,
+                 trControl = ctrl_tr,
+                 verbose = FALSE)          
+#
+## (5) Save  Model
+saveRDS(gbm_fit, file = "my_gbm_model.rds")
+#
+## (6) Stop the cluster
+closeCluster(cl)
+mpi.quit()
+#
+## (7) Exit R
+q()
